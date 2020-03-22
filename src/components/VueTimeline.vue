f060c6babb6c74376bd1ed69b4affa37295d64d4
0xdv <0xc0ffec0de@gmail.com>
2020-03-22 15:37:41 +0200

    added prop

--- src/components/VueTimeline.vue
+++ src/components/VueTimeline.vue
@@ -1,5 +1,5 @@
 <template>
-    <div><div id='timeline'></div></div>
+    <div id='timeline'></div>
 </template>
 
 <script>
@@ -7,23 +7,15 @@ import * as d3 from 'd3'
 
 import timeline from '../graph/timeline'
 
-let eventData = [{
-    id: 1,
-    name: "ddd",
-    start: new Date(2001, 1,1),
-    end: new Date(2020, 1,1),
-    position: 5
-}]
-
 export default {
-    data() {
-        return {
-
+    props: {
+        data: {
+            type: Array
         }
     },
     mounted() {
         d3.select('#timeline')
-            .datum(eventData)
+            .datum(this.data)
             .call(timeline({
                     widthResizable: true,
                     viewHeight: 300,
